58a2d4823484e801b6320501091ac80b743ef037
Gonzalo Melo <gonzalo@kona.tech>
2019-06-08 10:39:39 -0300

    Remove unused data

--- db/src/sample_data/data.sql
+++ db/src/sample_data/data.sql
@@ -16,21 +16,10 @@ COPY data.user (id,name,email,"password") FROM STDIN (FREEZE ON);
 2	bob	bob@email.com	pass
 \.
 -- 
--- fill table data.todo (6)
-\echo # filling table data.todo (6)
-COPY data.todo (id,todo,private,owner_id) FROM STDIN (FREEZE ON);
-1	item_1	FALSE	1
-2	item_2	TRUE	1
-3	item_3	FALSE	1
-4	item_4	TRUE	2
-5	item_5	TRUE	2
-6	item_6	FALSE	2
-\.
--- 
+
 -- restart sequences
 ALTER SEQUENCE data.user_id_seq RESTART WITH 3;
-ALTER SEQUENCE data.todo_id_seq RESTART WITH 7;
 -- 
+
 -- analyze modified tables
 ANALYZE data.user;
-ANALYZE data.todo;
